8cd1de9c18629382f47abaa8952a1ca1bd10da65
Clarence2810 <46648176+Clarence2810@users.noreply.github.com>
2020-09-07 07:28:34 +0800

    fixing and adding

--- src/clarence2810/extinguish/Main.php
+++ src/clarence2810/extinguish/Main.php
@@ -5,10 +5,9 @@ namespace clarence2810\extinguish;
 use pocketmine\{
 	Player,
 	Server,
-	plugin\PluginBase
+	plugin\PluginBase,
 	command\CommandSender,
 	command\Command,
-	utils\Textformat as C,
 	utils\Config,
 };;
 class Main extends PluginBase
@@ -16,24 +15,38 @@ class Main extends PluginBase
 	public function onEnable(){
 		$this->saveDefaultConfig();
         $this->getResource("config.yml");
-		$this->getLogger()->info("Plugin by Clarence2810 has been enabled!");
 	}
 	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args):bool{
 		switch($cmd->getName()){
 			case "extinguish":
-				if($sender instanceof Player){
-					if($sender->hasPermission("extinguish.command")){
+				if($sender->hasPermission("extinguish.command")){
+					if(empty($args[0])){
 						if($sender->isOnFire()){
 							$sender->extinguish();
-							$sender->sendMessage($this->getConfig()->get("extinguish-success"));
+							$sender->sendMessage($this->getConfig()->get("extinguished-success"));
 						}else{
 							$sender->sendMessage($this->getConfig()->get("not-on-fire"));
 						}
 					}else{
-						$sender->sendMessage($this->getConfig()->get("no-permission"));
+						if(!$sender->hasPermission("extinguish.command.other")){
+							$sender->sendMessage($this->getConfig()->get("other-no-permission"));
+						}else{
+							$player = $sender->getServer()->getPlayer($args[0]);
+							if($player !== null){
+								if($player->isOnFire()){
+									$player->extinguish();
+									$player->sendMessage(str_replace(["{sender}"], [$sender->getName()], $this->getConfig()->get("player-extinguished-success")));
+									$sender->sendMessage(str_replace(["{player}"], [$player->getName()], $this->getConfig()->get("sender-extinguished-success")));
+								}else{
+									$sender->sendMessage(str_replace(["{player}"], [$player->getName()], $this->getConfig()->get("other-not-on-fire")));
+								}
+							}else{
+								$sender->sendMessage($this->getConfig()->get("player-not-found"));
+							}
+						}
 					}
 				}else{
-					$sender->sendMessage($this->getConfig()->get("player-only"));
+					$sender->sendMessage($this->getConfig()->get("no-permission"));
 				}
 			break;
 		}
